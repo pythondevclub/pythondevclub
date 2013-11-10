f4983c8845f7f3fa6120b8c326dbd72dbc408b36
Alexandra <flutesa@ya.ru>
2013-11-10 21:58:26 +0400

    delete old folder

--- 1 lesson (arithmetic, i:o)/burkova/ZadachaD5.py
+++
@@ -1,7 +0,0 @@
-first_digit = int(input())
-second_digit = int(input())
-
-is_first_digit_max = (first_digit // second_digit + 2) // (first_digit // second_digit + 1) % 2
-is_second_digit_max = (is_first_digit_max + 1) % 2
-
-print(first_digit * is_first_digit_max + second_digit * is_second_digit_max)
